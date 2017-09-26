4b43a00f5004ce2591fb9db33215401958954eac
Alan Paul <allanpaulthe@gmail.com>
2017-09-26 17:10:31 +0000

    Add files via upload

---
+++ static/js/fun.js
@@ -0,0 +1,15 @@
+
+          function loadpic() {
+            document.getElementById("imgg").style.width="15vmax";
+          }
+          function closepic() {
+            document.getElementById("imgg").style.width="4vmax";
+            document.getElementById("bodymain").style.width="auto";
+          }
+          function addclass(){
+            document.getElementById("social").classList.add("animated", "bounce")
+          }
+          function removeclass(){
+            document.getElementById("social").classList.remove("animated", "bounce")
+          }
+          
