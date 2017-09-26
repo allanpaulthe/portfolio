33b422ef03cfa167f51e8386f4eeb0ae22f3b77c
Alan Paul <allanpaulthe@gmail.com>
2017-09-26 17:08:51 +0000

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, session, redirect, url_for, escape, request, render_template,flash,send_from_directory
+import os
 app = Flask(__name__)
 
 
@@ -6,5 +7,9 @@ app = Flask(__name__)
 def userhome():
     return render_template("index.html")
 
+@app.route('/projects')
+def projects():
+    return render_template("projects.html")
+
 if __name__ == '__main__':
-	app.run(debug = True)
+	app.run(debug=True)
